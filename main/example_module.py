98a79319368ac154ed4f97b7d1e559c082812a80
Ben Sherlock <github@bensherlock.co.uk>
2020-09-04 17:30:32 +0100

    Initial import of example file.

---
+++ main/example_module.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+#
+# MicroPython OTA Updater Example Module
+#
+# This file is part of micropython-ota-updater-example-module. 
+# https://github.com/bensherlock/micropython-ota-updater-example-module
+#
+#
+# MIT License
+#
+# Copyright (c) 2020 Benjamin Sherlock <benjamin.sherlock@ncl.ac.uk>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+"""MicroPython OTA Updater Example Module."""
+
+
+class ExampleModuleClass:
+    """An Example Module Class"""
+	
+    def __init__(self):
+        continue
+    
+    def __call__(self):
+        return self
+
+    def do_something(self):
+        """Do something."""
+        print("Hello from Example Module Class.")
